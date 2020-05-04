7c644e9d0aaf28bd176f9db1512ff3c333864c3d
Suzu Mikhail <codeotakuchiyan@gmail.com>
2020-05-04 20:54:10 +0800

    Add some data structs

---
+++ dsbrush/basic_brush.py
@@ -0,0 +1,13 @@
+
+
+class BasicBrush:
+    
+    def __init__(self):
+        self.size = 10
+        self.min_size_percent = 0
+        self.max_size_percent = 100
+        self.min_thickness_percent = 0
+        self.max_thickness_percent = 100
+        self.blur_percent = 0
+        self.texture_path = ""
+        self.texture_strength_percent = 0
---
+++ dspalette/colorwheel.py
@@ -0,0 +1,11 @@
+
+class ColorWheel:
+    def __init__(self):
+        self.r = 0
+        self.g = 0
+        self.b = 0
+        self.transparent = False
+    
+    def rgb2hsv(self, r, g, b):
+        return
+
